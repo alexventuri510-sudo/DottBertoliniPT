af24cad0ffe19a5290b76950eb64bb4a6bd3ffd7
alexventuri510-sudo <alexventuri510@gmail.com>
2026-08-24 11:30:26 +0200

    Miglioramento calcolatore alimenti con filtro valori nutrizionali

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2103742000" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2806690763" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
